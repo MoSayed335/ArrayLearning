14e15c2cec5eff16e911199d9b6e2a33f74b3db2
Mohamed Sayed <mo.sayed2037@gmail.com>
2026-07-13 09:44:41 +0300

    Add project files.

---
+++ Array/Leetcode/Longest-Common-Prefix.cs
@@ -0,0 +1,48 @@
+﻿//Write a function to find the longest common prefix string amongst an array of strings.
+
+//If there is no common prefix, return an empty string "".
+
+ 
+
+//Example 1:
+
+//Input: strs = ["flower", "flow", "flight"]
+//Output: "fl"
+//Example 2:
+
+//Input: strs = ["dog", "racecar", "car"]
+//Output: ""
+//Explanation: There is no common prefix among the input strings.
+
+
+namespace Array.Leetcode
+{
+    public class Longest_Common_Prefix
+    {
+        public class Solution
+        {
+            //strs = {"Flow","Fly"}
+            public string LongestCommonPrefix(string[] strs)
+            {
+                string prefix = strs[0];//Flow
+                for(int i = 1; i < strs.Length; i++) //i<2; 1 degit
+                {
+                    string currentprefix = string.Empty;
+                    for(int j = 0; j < strs[i].Length; j++)
+                    {
+                        if(j >= prefix.Length) break; //j>=1
+
+                        if (strs[i][j] == prefix[j])
+                        {
+                            currentprefix += strs[i][j];
+                        }
+                        else
+                            break;
+                    }
+                    prefix = currentprefix;
+                }
+              return prefix;
+            }
+        }
+    }
+}
---
+++ Array/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
+            for(int i=0;  i<nums.Length; i++)
+            {
+                //new constran
+                int[] node = { nums[i] %10 };
+                Console.WriteLine(node[i]);
+            }
+        }
+    }
+}
